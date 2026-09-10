9c5ff711cc4eaa8b31fd63fe9e9e425a21e3ecb8
dingyouqiang1996 <128674406+dingyouqiang1996@users.noreply.github.com>
2026-09-10 17:05:48 +0800

    2026-9-10 17:03:42026-9-10 17:05:458

--- env.sh
+++ env.sh
@@ -2,11 +2,11 @@
 DOCKER_DOMAIN="hkccr.ccs.tencentyun.com"
 DOCKER_NAMESPACE="rasp"
 DOCKER_REGISTRY="ci"
-DOCKER_IMAGETAG="yt-dlp"
+DOCKER_IMAGETAG="rclone"
 DOCKER_VERSIONTAG="v7_192.168.0.62"
 DOCKER_ARCH="arm64"
 #----------------------------------------------------------#
 DOCKER_FULL_PUSH_URL="${DOCKER_DOMAIN}/${DOCKER_NAMESPACE}/${DOCKER_REGISTRY}:${DOCKER_IMAGETAG}_${DOCKER_ARCH}_${DOCKER_VERSIONTAG}"
 #----------------------------------------------------------#
-MY_FOLDER="yt-dlp"
+MY_FOLDER="rclone"
 #----------------------------------------------------------#
