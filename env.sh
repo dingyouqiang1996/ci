2954b037f671304c827ea7c12e8a1457ba9677b5
dingyouqiang1996 <128674406+dingyouqiang1996@users.noreply.github.com>
2026-09-13 16:19:42 +0800

    2026-09-13 16:19:40

--- env.sh
+++ env.sh
@@ -1,12 +1,12 @@
 #----------------------------------------------------------#
 DOCKER_DOMAIN="ccr.ccs.tencentyun.com"
 DOCKER_NAMESPACE="rasp-gz"
-DOCKER_REGISTRY="ci"
+DOCKER_REGISTRY="bt"
 DOCKER_IMAGETAG="rclone"
 DOCKER_VERSIONTAG="v1"
 DOCKER_ARCH="arm64"
 #----------------------------------------------------------#
 DOCKER_FULL_PUSH_URL="${DOCKER_DOMAIN}/${DOCKER_NAMESPACE}/${DOCKER_REGISTRY}:${DOCKER_IMAGETAG}_${DOCKER_ARCH}_${DOCKER_VERSIONTAG}"
 #----------------------------------------------------------#
-MY_FOLDER="rclone"
+MY_FOLDER="bt"
 #----------------------------------------------------------#
